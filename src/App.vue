ee9ee81092eb0bd52f8d508d72bd121b6dcd816c
Wright <calebogundiya@gmail.com>
2020-09-26 15:08:02 +0100

    Persist change in database through endpoint

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
+    <p>Logo</p>
+    <p><span>Home</span> <span>About Us</span></p>
     <h1>Items Management Page</h1>
-    <form @submit.prevent="addToListIn">
-      <input
-        type="text"
-        v-model="listMember"
-        placeholder="Enter Item Name and Click Add"
-      />
-      <button type="submit" @click="addToListIn">Add</button>
-    </form>
 
     <div class="dataIn">
+      <form @submit.prevent="addToListIn">
+        <input
+          type="text"
+          v-model="listMember"
+          placeholder="Enter Item Name and Click Add"
+        />
+        <button type="submit" @click="addToListIn">Add</button>
+      </form>
       <ul>
         <li
           v-for="(list, index) in listIn"
@@ -20,10 +22,13 @@
           {{ list }}
         </li>
       </ul>
-      <button>➡️</button>
     </div>
-
+    <div>
+      <button>➡️</button>
+      <button>⬅️</button>
+    </div>
     <div class="dataOut">
+      <h1>Selected Items</h1>
       <ul>
         <li
           v-for="(list, index) in listOut"
@@ -33,8 +38,10 @@
           {{ list }}
         </li>
       </ul>
-      <button type="submit">⬅️</button>
     </div>
+
+    <hr />
+    <p><span>Home</span> <span>About Us</span></p>
   </div>
 </template>
 
@@ -47,23 +54,52 @@ export default {
       selectedMember: 0,
       listIn: [],
       listOut: [],
+      dataUrl: "https://items-manager-api.herokuapp.com/",
     };
   },
   methods: {
+    async fetchList() {
+      let itemsList = await fetch(`${this.dataUrl}`);
+      let response = await itemsList.json();
+      console.log(response);
+      this.listIn = response[0].in;
+      this.listOut = response[0].out;
+    },
+    async postList(data) {
+      let response = await fetch(`${this.dataUrl}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: data,
+      });
+
+      return await response.json();
+    },
     addToListIn() {
       this.listIn.push(this.listMember);
       this.listMember = "";
     },
+    persistItems() {
+      let data = JSON.stringify({ in: this.listIn, out: this.listOut });
+      let response = this.postList(data);
+      console.log(response);
+    },
     moveRight(index) {
       let outBoundValue = this.listIn.splice(index, 1);
       this.listOut.push(outBoundValue[0]);
+      this.persistItems();
     },
     moveLeft(index) {
       let inBoundValue = this.listOut.splice(index, 1);
       this.listIn.push(inBoundValue[0]);
+      this.persistItems();
     },
   },
   components: {},
+  mounted: function() {
+    this.fetchList();
+  },
 };
 </script>
 
@@ -76,10 +112,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   text-align: left;
+  margin: auto;
+  width: 900px;
 }
 
 ul {
   text-decoration: none;
   list-style-type: none;
+}
+
+.dataIn,
+.dataOut {
+  width: 50%;
 }
 </style>
